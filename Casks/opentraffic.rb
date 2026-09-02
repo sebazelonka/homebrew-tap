cf959d5c94083b0548877002e35f011360e03c2b
open-traffic-ci <955591+sebazelonka@users.noreply.github.com>
2026-09-02 12:37:46 +0000

    opentraffic 1.1.0

--- Casks/opentraffic.rb
+++ Casks/opentraffic.rb
@@ -1,6 +1,6 @@
 cask "opentraffic" do
-  version "1.0.5"
-  sha256 "c2a32ca504f13db1b9459dfe4c1e14746430fbfb0d0f5a910cadda350e604428"
+  version "1.1.0"
+  sha256 ""
 
   url "https://open-traffic-site.vercel.app/downloads/OpenTraffic.dmg"
   name "OpenTraffic"
